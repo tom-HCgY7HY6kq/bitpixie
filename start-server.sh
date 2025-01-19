d0734284e54176fee7a1a8d545575b036282ff21
Andreas Grasser <andi@grasserisen.de>
2025-01-19 01:30:55 +0100

    Server Start Script edited

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# TODO: Inferface abfragen
-$interface="eth0"
+if [[ "$1" = "" ]]
+then
+  echo "Usage: ./start-server.sh <interface>"
+  exit
+fi
 
-sudo dnsmasq --no-daemon --interface=$interface --dhcp-range=10.13.37.100, 10.13.37.101, 255.255.255.0, 1h --dhcp-boot=bootmgfw.efi --enable-tftp --tftp-root=PXE-Server --log-dhcp
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+interface=$1
+
+sudo ip a add 10.13.37.100/24 dev $interface
+
+sudo dnsmasq --no-daemon --interface=$interface --dhcp-range=10.13.37.100,10.13.37.101,255.255.255.0,1h --dhcp-boot=bootmgfw.efi --enable-tftp --tftp-root=$SCRIPTPATH/PXE-Server --log-dhcp
