30a123c800f5201d6a4b74e5fb7976d4c3cd927e
Andreas Grasser <andi@grasserisen.de>
2025-01-20 23:03:06 +0100

    Create BCD Improved

--- create-bcd.bat
+++ create-bcd.bat
@@ -1,12 +1,13 @@
 @echo off
 
-bcdedit /export BCD_modded
+SET /P DRIVE=Set the drive letter where the BCD file is located:
 
-copy BCD_modded C:\BCD_unmodded
+bcdedit /export BCD_modded
 
 bcdedit /store BCD_modded /create /d "softreboot" /application startup>GUID.txt
 For /F "tokens=2 delims={}" %%i in (GUID.txt) do (set REBOOT_GUID=%%i)
 del guid.txt
+
 bcdedit /store BCD_modded /set {%REBOOT_GUID%} path "\shimx64.efi"
 bcdedit /store BCD_modded /set {%REBOOT_GUID%} device boot
 bcdedit /store BCD_modded /set {%REBOOT_GUID%} pxesoftreboot yes
@@ -17,6 +18,28 @@ bcdedit /store BCD_modded /set {default} path "\\"
 bcdedit /store BCD_modded /set {default} winpe yes
 
 bcdedit /store BCD_modded /displayorder {%REBOOT_GUID%} /addlast
-move BCD_modded C:\BCD_modded
+
+
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Do you want to move the file to the SMB server on 10.13.37.1 (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+SET /P DRIVE=Set the drive letter for the SMB share to be mounted:
+net use %drv%: \\10.13.37.1\smb
+move BCD_modded %drv%:\BCD
+
+goto :EOF
+
+:END
+
+move BCD_modded C:\BCD
+echo "BCD file was moved to C:\BCD"
+
+goto :EOF
+
+endlocal
+
+:EOF
 
 pause
